e95da2a1025907baa892ffc47ca8c582809497e8
Martin Jaensson <martin.jaensson@exsitec.se>
2017-08-08 23:42:10 +0200

    testning skript

--- deploy.cmd
+++ deploy.cmd
@@ -1,3 +1,1 @@
-cd app
-npm install && npm run build:prod 
-&& del /q D:\home\site\wwwroot\* && for /d %%x in (D:\home\site\wwwroot\*) do @rd /s /q "%%x" && xcopy /s D:\home\site\repository\app\dist D:\home\site\wwwroot
+del /q D:\home\site\wwwroot\* && for /d %%x in (D:\home\site\wwwroot\*) do @rd /s /q "%%x" && xcopy /s D:\home\site\repository\app\dist D:\home\site\wwwroot
