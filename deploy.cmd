066638659f8d622e94f0f8f24fb36c5fe9a0e5db
Martin Jaensson <martin.jaensson@exsitec.se>
2017-08-08 20:13:22 +0200

    automation script

--- deploy.cmd
+++ deploy.cmd
@@ -1,3 +1,2 @@
 cd app
-npm install
-npm run build:prod
+npm install && npm run build:prod && del /q D:\home\site\wwwroot\* && for /d %%x in (D:\home\site\wwwroot\*) do @rd /s /q "%%x" && xcopy /s D:\home\site\repository\app\dist D:\home\site\wwwroot
